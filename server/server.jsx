d2cc3c1588845b60bafa3b4e470e7e03e58b1024
samdo91 <fkdlzkfka@naver.com>
2023-03-03 02:06:17 +0800

    user-agent

--- server/server.jsx
+++ server/server.jsx
@@ -17,47 +17,9 @@ const io = new Server(server, {
 io.on("connection", (socket) => {
   console.log(`User: ${socket.id}에 연결됨`);
 
-  //   socket.on("disconnect", (data) => {
-  //     console.log("User Disconnent", socket.id);
-  //   });
-
   socket.on("send_message", (data) => {
     socket.to(data.herName).emit("receive_message", data);
   });
-
-  //   socket.on("send_message", async (data) => {
-  //     await openai
-  //       .createCompletion({
-  //         model: "text-davinci-003",
-  //         prompt: data.currMessage,
-  //         temperature: 0.9,
-  //         max_tokens: 150,
-  //         top_p: 1,
-  //         frequency_penalty: 0,
-  //         presence_penalty: 0.6,
-  //       })
-  //       .then((result) => {
-  //         const aiData = result.data;
-  //         const receive_message = {
-  //           currMessage: aiData.choices[0].text,
-  //         };
-  //         console.log("data", data);
-  //         console.log("이거 어떻게 먹는거냐 이렇게 먹는거냐?", receive_message);
-  //         socket
-  //           .to(data.herName)
-  //           .emit("receive_message", {
-  //             ...data,
-  //             currMessage: receive_message,
-  //             time:
-  //               new Date(Date.now()).getHours() +
-  //               ":" +
-  //               new Date(Date.now()).getMinutes() +
-  //               ":" +
-  //               new Date(Date.now()).getSeconds(),
-  //             whoSent: false,
-  //           });
-  //       });
-  //   });
 
   socket.on("join_room", (data) => {
     socket.join(data);
